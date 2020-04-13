54ba9f4d9567b7a49d9dcba78540e1cdc94db562
Simon Lin <simonlin@google.com>
2020-04-13 22:03:40 +0800

    add doc for cmd/otns

--- cmd/otns/otns.go
+++ cmd/otns/otns.go
@@ -24,6 +24,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+// otns is a program that simulates Thread networks with OpenThread virtual time simulations.
 package main
 
 import (
