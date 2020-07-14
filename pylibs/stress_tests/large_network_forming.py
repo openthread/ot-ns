4c0e0a0463d4e0950df5fa4c32d279e532327be1
Simon Lin <simonlin@google.com>
2020-07-14 23:51:13 +0800

    rename to large_network_forming

---
+++ pylibs/stress_tests/large_network_forming.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# Copyright (c) 2020, The OTNS Authors.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the
+#    names of its contributors may be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+import os
+import time
+
+from BaseStressTest import BaseStressTest
+
+XGAP = 100
+YGAP = 100
+RADIO_RANGE = int(XGAP * 1.5)
+
+LARGE_N = 8
+
+SIMULATE_TIME = 60
+REPEAT = 100 if os.getenv('GITHUB_ACTIONS') else 1
+
+
+class StressTest(BaseStressTest):
+    SUITE = 'network-forming'
+
+    def __init__(self):
+        super(StressTest, self).__init__("Large Network Formation Test",
+                                         ["Simulation Time", "Execution Time", "Average Partition Count in 60s"])
+
+    def run(self):
+        self.ns.packet_loss_ratio = 0.2
+
+        durations = []
+        partition_counts = []
+        for _ in range(REPEAT):
+            dt, par_cnt = self.test_n(LARGE_N)
+            durations.append(dt)
+            partition_counts.append(par_cnt)
+
+        self.result.append_row('%ds' % (SIMULATE_TIME * REPEAT), '%ds' % sum(durations),
+                               '%d' % (sum(partition_counts) / len(partition_counts)))
+
+    def test_n(self, n):
+        self.reset()
+
+        for r in range(n):
+            for c in range(n):
+                self.ns.add("router", 50 + XGAP * c, 50 + YGAP * r, radio_range=RADIO_RANGE)
+
+        t0 = time.time()
+        self.ns.go(SIMULATE_TIME)
+        dt = time.time() - t0
+        return dt, len(self.ns.partitions())
+
+
+if __name__ == '__main__':
+    StressTest().run()
---
+++ pylibs/stress_tests/run_stress_suite.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# Copyright (c) 2020, The OTNS Authors.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the
+#    names of its contributors may be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import inspect
+import logging
+import sys
+import os
+
+
+def find_stress_test_classes(mod, suite_name: str):
+    from BaseStressTest import BaseStressTest
+
+    sts = []
+    for name, member in inspect.getmembers(mod):
+        if not isinstance(member, type):
+            continue
+
+        if issubclass(member, BaseStressTest) and member is not BaseStressTest:
+            assert hasattr(member, 'SUITE') and isinstance(member.SUITE,
+                                                           str), f'Please define `SUITE` for {member.__name__}'
+            if member.SUITE == suite_name:
+                sts.append(member)
+
+    return sts
+
+
+def main():
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+
+    script_path = sys.argv[0]
+    script_dir = os.path.abspath(os.path.dirname(script_path))
+    logging.info('script directory: %s' % script_dir)
+
+    suite_names = sys.argv[1:]
+    if not suite_names:
+        logging.error("suite names not specified, nothing to do")
+        exit(-1)
+
+    logging.info("Running stress test suite: %s ...", ', '.join(suite_names))
+
+    sys.path.insert(0, script_dir)
+
+    for suite_name in suite_names:
+        run_suite(script_dir, suite_name)
+
+
+def run_suite(script_dir, suite_name: str):
+    stress_tests = []
+    for filename in os.listdir(script_dir):
+        if not filename.endswith('.py'):
+            continue
+
+        modname = os.path.splitext(filename)[0]
+        mod = __import__(modname)
+        stress_test_classes = find_stress_test_classes(mod, suite_name)
+        if not stress_test_classes:
+            continue
+
+        stress_tests.append((filename, stress_test_classes))
+
+    for filename, clses in sorted(stress_tests):
+        for cls in clses:
+            t = cls()
+            logging.info("Running stress test: %s ...", t.name)
+            t.run(report=True)
+
+
+if __name__ == '__main__':
+    main()
