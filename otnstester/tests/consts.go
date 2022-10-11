ce79b088d370d7543f67e80f9146ec0584ada80c
Esko Dijk <esko.dijk@iotconsultancy.nl>
2022-10-11 09:13:54 +0200

    [otnstester] fix compilation

--- otnstester/tests/consts.go
+++ otnstester/tests/consts.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, The OTNS Authors.
+// Copyright (c) 2020-2022, The OTNS Authors.
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -26,7 +26,7 @@
 
 package main
 
-import "github.com/openthread/ot-ns/simulation"
+import "github.com/openthread/ot-ns/types"
 
 const (
 	RoleLeader = "leader"
@@ -35,5 +35,5 @@ const (
 )
 
 var (
-	DefaultRadioRange = simulation.DefaultNodeConfig().RadioRange
+	DefaultRadioRange = types.DefaultNodeConfig().RadioRange
 )
