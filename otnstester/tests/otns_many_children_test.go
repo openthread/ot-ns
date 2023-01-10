1b9b27413abe250ce6ae62c8adc55df23759130c
Esko Dijk <esko.dijk@iotconsultancy.nl>
2023-01-10 11:53:04 +0100

    [otnstester] Split basic Go test into 3 separate files to enable multiple tests; adapted Many-Children test; added basic 1-router test.

--- otnstester/tests/otns_many_children_test.go
+++ otnstester/tests/otns_many_children_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2023, The OTNS Authors.
+// Copyright (c) 2023, The OTNS Authors.
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -27,59 +27,43 @@
 package main
 
 import (
+	"math"
+	"math/rand"
 	"testing"
 	"time"
 
 	"github.com/openthread/ot-ns/otnstester"
 )
 
-func TestAdd(t *testing.T) {
+func TestAddManySEDs(t *testing.T) {
 	ot := otnstester.NewOtnsTest(t)
 	defer ot.Shutdown()
-	testAddNode(ot)
+	testAddManySEDs(ot)
 }
 
-func testAddNode(test *otnstester.OtnsTest) {
+func testAddManySEDs(test *otnstester.OtnsTest) {
 	test.Reset()
+	//test.Command("radiomodel Ideal")
 
-	nodeid := test.AddNode("router", 100, 100)
+	x := 500
+	y := 500
+	nodeid := test.AddNode("router", x, y)
 	test.ExpectTrue(nodeid == 1)
 	test.Go(time.Second * 10)
 	test.ExpectTrue(test.GetNodeState(nodeid) == RoleLeader)
-	test.ExpectVisualizeAddNode(nodeid, 100, 100, DefaultRadioRange)
 
-	router2 := test.AddNode("router", 120, 120)
-	test.ExpectTrue(router2 == 2)
-	test.Go(time.Second * 10)
-	test.ExpectTrue(test.GetNodeState(router2) == RoleRouter)
-	test.ExpectVisualizeAddNode(router2, 120, 120, DefaultRadioRange)
+	N := 12
+	var r float64
+	for n := 1; n <= N; n++ {
+		fra := float64(n) / float64(N)
+		r = rand.Float64()*80.0 + 100.0
+		nodeid = test.AddNodeRr("med", int(float64(x)+r*math.Sin(2.0*math.Pi*fra)),
+			int(float64(y)+r*math.Cos(2.0*math.Pi*fra)), 200)
+		test.Go(time.Millisecond * 2200)
+	}
+	test.Go(time.Second * 60)
 
-	test.Command("add fed x 50 y 60")
-	fed := 3
-	test.Go(time.Second * 10)
-	test.ExpectTrue(test.GetNodeState(fed) == RoleChild)
-	test.ExpectVisualizeAddNode(fed, 50, 60, DefaultRadioRange)
-
-	fedInfo := test.ListNodes()[fed]
-	test.ExpectTrue(fedInfo.X == 50)
-	test.ExpectTrue(fedInfo.Y == 60)
-
-	test.Command("add med x 10 y 20 rr 121")
-	med := 4
-	test.Go(time.Second * 10)
-	test.ExpectTrue(test.GetNodeState(med) == RoleChild)
-	test.ExpectVisualizeAddNode(med, 10, 20, 121)
-
-	medInfo := test.ListNodes()[med]
-	test.ExpectTrue(medInfo.X == 10)
-	test.ExpectTrue(medInfo.Y == 20)
-
-	test.Command("add sed x 30 y 40")
-	sed := 5
-	test.Go(time.Second * 10)
-	test.ExpectTrue(test.GetNodeState(sed) == RoleChild)
-	test.ExpectVisualizeAddNode(sed, 30, 40, DefaultRadioRange)
-	sedInfo := test.ListNodes()[sed]
-	test.ExpectTrue(sedInfo.X == 30)
-	test.ExpectTrue(sedInfo.Y == 40)
+	for n := 2; n <= N+1; n++ {
+		test.ExpectTrue(test.GetNodeState(n) == RoleChild)
+	}
 }
