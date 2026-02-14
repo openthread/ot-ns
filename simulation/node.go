8fbda751dbb53f0c2d8643ca33718c9fe4e843ba
Esko Dijk <esko.dijk@iotconsultancy.nl>
2026-02-14 21:11:24 +0100

    [simulation] fix flash filename for sim-IDs > 0
    This fix adds the PORT_OFFSET environment var to each started node, such that
    it receives the correct sim-ID to use for the .flash filename.
    
    Close #694

--- simulation/node.go
+++ simulation/node.go
@@ -102,6 +102,7 @@ func newNode(s *Simulation, nodeid NodeId, cfg *NodeConfig, dnode *dispatcher.No
 		seedParam := fmt.Sprintf("%d", cfg.RandomSeed)
 		cmd = exec.CommandContext(context.Background(), cfg.ExecutablePath, strconv.Itoa(nodeid), s.d.GetUnixSocketName(), seedParam)
 	}
+	cmd.Env = append(cmd.Env, fmt.Sprintf("PORT_OFFSET=%d", s.cfg.Id))
 
 	node := &Node{
 		S:             s,
