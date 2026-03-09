bbd54b3fe947af96bc32c69bd73fa49fe4ab8ed2
Esko Dijk <esko.dijk@iotconsultancy.nl>
2026-03-09 18:03:35 +0100

    [all] support for OT Posix node with RCP using realtime UART (#769)
    This adds support for running a standard OT Posix node in a realtime simulation, where
    the Posix process performs realtime SPINEL/HDLC UART communication with an RCP process.
    Support for RCPs is the basis for upcoming features like including real OTBRs in a
    simulation, either running on the local host or running in a Docker.
    
    The dispatcher/simulation core is updated to allow handling of both 'realtime UART'
    (stdout) based CLI and logging interactions and 'virtual-time UART' interactions by
    nodes, in the same simulation. The real-time simulation mode now has a different time
    keeping mechanism that is synced to the host's clock and processes continuously, not
    based on discrete time periods or different speeds - like virtual-time simulation is.
    Deletion of nodes is updated in real-time mode: it is ongoing in the background in a
    separate goroutine, to ensure the simulation doesn't halt upon node deletion and that
    all final log messages of the exiting node are captured also.
    
    A method waitForSimulation() is introduced to be more clear when command processing
    needs to wait for simulation events to be processed first. It simplifies the code
    in expectEvent() and expectLine() functions also.
    
    Also, the queueing and filtering of UART/log output of OT nodes is simplified to make
    the code better readable and the UART data flow easier to understand and debug. Log
    lines are received over stderr from the OT Posix node. The logger package (parse.go)
    is updated to parse syslog style log lines, and unit tests are added for all parsing
    functions.
    
    The CLI (over PTY/UART/...) echo behavior differs for Linux Posix nodes that are
    controlled via stdin/stdout: there is no 'echo' behavior. Depending on OS, there could
    be echo. To handle all possible combinations of platform/node-type, there is a new function
    'setupCli' added that autodetects whether echo is used for a node, or not, and whether
    the CLI connection is functional.
    
    The RFSIM radio C code is updated to avoid going to sleep when there are still OTNS
    events in the queue, received via the Unix socket. This ensures faster overall
    processing with less events being sent, relevant for nodes to keep up in real-time
    mode.
    
    Python unit tests are extended with a more strict error checking, to ensure that
    unexpected errors from OTNS cause the unit tests to fail.
    
    Finally, previous build errors for the rcp target (Issue #758) are now resolved.
    
    Close #758
    
    Co-authored-by: François Michel <francoismichel.dev@gmail.com>

--- ot-rfsim/src/radio.c
+++ ot-rfsim/src/radio.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2016-2024, The OpenThread Authors.
+ *  Copyright (c) 2016-2026, The OpenThread Authors.
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
